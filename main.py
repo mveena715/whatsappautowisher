bdb4e8f79ce660114e3519a6af717de11aacb682
mveena715 <91141330+mveena715@users.noreply.github.com>
2021-10-01 22:29:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import pywhatkit
+try:
+    pywhatkit.sendwhatmsg('+91 9945142310','HAPPY BIRTHDAY ,MAY ALL YOUR DREAMS COME TRUE ',21,7)
+    print("successfully sent")
+except:
+    print("an error")
